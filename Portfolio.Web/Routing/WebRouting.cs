68cad1fb9e522ee83df3417eff1ebdef5300e4f7
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-09-15 15:13:12 +0200

    Better routes handling

---
+++ Portfolio.Web/Routing/WebRouting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Web.Routing
+{
+    public static class WebRouting
+    {
+        public const string Counter = "counter";
+        public const string FetchData = "fetchdata";
+    }
+}
