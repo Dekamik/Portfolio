b4bd9bd3ba9acfb59c5d8d23413ffcb5cf461242
Dennis von Bargen <dennis@vonbargen.se>
2020-09-15 19:26:04 +0200

    Added basic portfolio pages

---
+++ Portfolio.Web/Routing/WebRoutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Web.Routing
+{
+    public static class WebRoutes
+    {
+        public const string AboutMe = "aboutme";
+
+        public const string Projects = "projects";
+
+        public const string Contact = "contact";
+    }
+}
