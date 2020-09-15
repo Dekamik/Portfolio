dbc8ff32227801662b881ac8dd94f70c9a2cf699
Dennis von Bargen <dennis@vonbargen.se>
2020-09-15 19:55:01 +0200

    Migrated About page from react site

---
+++ Portfolio.Common/Helpers/DateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Common.Helpers
+{
+    public static class DateHelper
+    {
+        public static int GetYearsBetween(DateTime from, DateTime to)
+        {
+            if (from > to)
+            {
+                throw new ArgumentOutOfRangeException("To must be a date after from");
+            }
+            return (int)Math.Floor((to - from).TotalDays / 365.25D);
+        }
+    }
+}
