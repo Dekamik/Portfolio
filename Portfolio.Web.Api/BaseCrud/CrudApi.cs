c01da9f33c55aebc56571ccd3a391654e5626fb4
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-09-16 10:56:56 +0200

    Created CrudControllers for repository pattern

---
+++ Portfolio.Web.Api/BaseCrud/CrudApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Portfolio.Core.Entities.Base;
+using Portfolio.Core.Repositories.Base;
+
+namespace Portfolio.Web.Api.BaseCrud
+{
+    public class CrudApi<TEntity, TRepository, TDbContext> : ReadOnlyRepositoryApi<TEntity, TRepository, TDbContext>
+        where TEntity : BaseEntity
+        where TRepository : Repository<TEntity, TDbContext>
+        where TDbContext : DbContext
+    {
+        public CrudApi(TRepository repository, ILogger logger) : base(repository, logger) { }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TEntity entity)
+        {
+            await _repository.Create(entity);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(TEntity entity)
+        {
+            await _repository.Update(entity);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            TEntity entity = _repository.Get(id).SingleOrDefault();
+
+            if (entity == default)
+            {
+                return NotFound();
+            }
+
+            await _repository.Delete(entity);
+            return Ok();
+        }
+    }
+}
---
+++ Portfolio.Web.Api/BaseCrud/ReadOnlyRepositoryApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Portfolio.Core.Entities.Base;
+using Portfolio.Core.Repositories.Base;
+
+namespace Portfolio.Web.Api.BaseCrud
+{
+    public abstract class ReadOnlyRepositoryApi<TEntity, TRepository, TDbContext> : ControllerBase
+        where TEntity : BaseEntity
+        where TRepository : ReadOnlyRepository<TEntity, TDbContext>
+        where TDbContext : DbContext
+    {
+        internal readonly TRepository _repository;
+        internal readonly ILogger _logger;
+
+        public ReadOnlyRepositoryApi(TRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_repository.Get(id));
+        }
+    }
+}
