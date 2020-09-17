e273e94fa422f6a9ea956bac8a635d65f636bc4c
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-09-17 13:44:43 +0200

    Created ClearDatabase.sql

---
+++ Portfolio.Core/BaseData/ClearDatabase.sql
@@ -0,0 +1,18 @@
+﻿USE Portfolio;
+
+BEGIN TRANSACTION;
+
+DELETE FROM ProjectSkills;
+DBCC CHECKIDENT ('Portfolio.dbo.ProjectSkills', RESEED, 0);
+
+DELETE FROM Skills;
+DBCC CHECKIDENT ('Portfolio.dbo.Skills', RESEED, 0);
+
+DELETE FROM Projects;
+DBCC CHECKIDENT ('Portfolio.dbo.Projects', RESEED, 0);
+
+DELETE FROM Employers;
+DBCC CHECKIDENT ('Portfolio.dbo.Employers', RESEED, 0);
+
+ROLLBACK;
+--COMMIT;
